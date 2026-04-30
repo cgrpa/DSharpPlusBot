8d4434a0843bbfceaea1aa746e98c1b1a43cbc50
Che <che@chesbrain.com>
2026-04-30 19:07:09 +0100

    update locals, git ignore, update pr validation - conditional, only runs terraform plan if tf changes. runs dotnet test always. fails pr if status check fails (either of depedent)

--- src/terraform/locals.tf
+++ src/terraform/locals.tf
@@ -4,6 +4,6 @@ locals {
         Application = var.application
         Location = var.location
     }
-    name_prefix = "${var.environment}-${var.application}"
-    name_prefix_no_dash = "${var.environment}${var.application}"
+    name_prefix = "${var.environment}-${var.location_short}-${var.application}"
+    name_prefix_no_dash = "${var.environment}${var.location_short}${var.application}"
 }
